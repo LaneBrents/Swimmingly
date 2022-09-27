4b9df06b06d2542c5382440b1dca99b7bf01f2ee
LaneBrents <lanebrents@gmail.com>
2022-09-27 15:33:14 -0400

    completed NewSwimmer form

--- src/components/NewSwimmer/NewSwimmer.jsx
+++ src/components/NewSwimmer/NewSwimmer.jsx
@@ -1,57 +1,69 @@
 import React, { useState } from "react";
 import { Button, Form, Segment } from "semantic-ui-react";
+import { useNavigate } from "react-router-dom";
 
 export default function NewSwimmerForm(props) {
-  // create the state, pay attention to how the inputs are setup!
-  const [state, setState] = useState({
-    caption: "",
-  });
-  // The function that handles the changes on the input, Look at the inputs for the name of it
-  const [selectedFile, setSelectedFile] = useState("");
 
-  function handleFileInput(e) {
-    console.log(e.target.files, " < - this is e.target.files!");
-    setSelectedFile(e.target.files[0]);
-  }
+    const navigate = useNavigate();
 
-  function handleChange(e) {
-    setState({
-      caption: e.target.value,
+    const [state, setState] = useState({
+        name: "",
+        grade: "",
+        gender: "",
     });
-  }
 
-  function handleSubmit(e) {
-    e.preventDefault();
 
-    const formData = new FormData();
-    formData.append("photo", selectedFile);
-    formData.append("caption", state.caption);
-    props.handleAddPost(formData); // formData is the data we want to send to the server!
-  }
+    function handleChange(e) {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value,
+        });
+    }
 
-  return (
-    <Segment>
-      <Form onSubmit={handleSubmit}>
-        <Form.Input
-          className="form-control"
-          name="name"
-          value={state.caption}
-          placeholder="Insert Swimmer Name"
-          onChange={handleChange}
-          required
-        />
-        <Form.Field>
-          <Form.Input
-            type="file"
-            name="photo"
-            placeholder="upload image"
-            onChange={handleFileInput}
-          />
-        </Form.Field>
-        <Button type="submit" className="btn">
-          ADD SWIMMER
-        </Button>
-      </Form>
-    </Segment>
-  );
+    async function handleSubmit(e) {
+
+        e.preventDefault();
+
+        try {
+            await props.handleAddSwimmer(state);
+            navigate("/MyTeamView");
+        } catch (err) {
+            console.log(err, "<- in AddSwimmer")
+        }
+    }
+
+
+    return (
+        <Segment>
+            <Form onSubmit={handleSubmit}>
+                <Form.Input
+                    className="form-control"
+                    name="name"
+                    value={state.caption}
+                    placeholder="Name"
+                    onChange={handleChange}
+                    required
+                />
+                 <Form.Input
+                    className="form-control"
+                    name="grade"
+                    value={state.caption}
+                    placeholder="Grade"
+                    onChange={handleChange}
+                    required
+                />
+                 <Form.Input
+                    className="form-control"
+                    name="gender"
+                    value={state.caption}
+                    placeholder="Gender"
+                    onChange={handleChange}
+                    required
+                />
+                <Button type="submit" className="btn">
+                    Add Swimmer
+                </Button>
+            </Form>
+        </Segment>
+    );
 }
